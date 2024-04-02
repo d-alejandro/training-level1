c90b383211a23b9d34d89f981ecdcca5950a50d7
Alexander Dadyka <sanekokokok@gmail.com>
2024-04-03 02:52:05 +0300

    Add: task #2_2

---
+++ t_2/main_2.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+/*
+Написать программу, которая конкурентно рассчитает значение квадратов чисел
+взятых из массива (2,4,6,8,10) и выведет их квадраты в stdout.
+*/
+
+type SquareCalculation struct {
+	sync.Mutex
+	Squares map[int]int
+}
+
+func NewSquareCalculation() *SquareCalculation {
+	squares := make(map[int]int)
+	return &SquareCalculation{Squares: squares}
+}
+
+func (receiver *SquareCalculation) Set(key int, square int) {
+	receiver.Lock()
+	defer receiver.Unlock()
+
+	receiver.Squares[key] = square
+}
+
+func main() {
+	const waitGroupDelta = 1
+
+	var waitGroup sync.WaitGroup
+
+	array := [5]int{2, 4, 6, 8, 10}
+
+	squareCalculation := NewSquareCalculation()
+
+	for _, value := range array {
+		waitGroup.Add(waitGroupDelta)
+
+		go func(value int) {
+			squareCalculation.Set(value, value*value)
+			waitGroup.Done()
+		}(value)
+	}
+
+	waitGroup.Wait()
+
+	for key, squares := range squareCalculation.Squares {
+		fmt.Println(key, "=>", squares)
+	}
+}
+
+/*
+10 => 100
+2 => 4
+4 => 16
+6 => 36
+8 => 64
+*/

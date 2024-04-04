f045e5f373d6831fa162a33d137cf5e94095ea5f
Alexander Dadyka <sanekokokok@gmail.com>
2024-04-04 18:27:45 +0300

    Add: task #6_2

--- t_6/main_1.go
+++ t_6/main_1.go
@@ -41,5 +41,6 @@ func main() {
 Goroutine run
 Goroutine run
 Goroutine run
+Goroutine run
 Goroutine stop
 */
---
+++ t_6/main_2.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+/*
+Реализовать все возможные способы остановки выполнения горутины.
+
+Через отдельный канал, получение данных из которого сигнализирует об остановке горутины
+*/
+
+func main() {
+	var waitGroup sync.WaitGroup
+
+	stopChannel := make(chan struct{})
+
+	waitGroup.Add(1)
+
+	go func(channel <-chan struct{}) {
+		stopFlag := false
+
+		for !stopFlag {
+			select {
+			case <-channel:
+				fmt.Println("Goroutine stop")
+				stopFlag = true
+			default:
+				fmt.Println("Goroutine run")
+				time.Sleep(time.Second)
+			}
+		}
+
+		waitGroup.Done()
+	}(stopChannel)
+
+	time.Sleep(3 * time.Second)
+
+	stopChannel <- struct{}{}
+	close(stopChannel)
+
+	waitGroup.Wait()
+}
+
+/*
+Goroutine run
+Goroutine run
+Goroutine run
+Goroutine stop
+*/

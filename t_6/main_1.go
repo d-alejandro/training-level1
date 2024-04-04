99799effb41c705cc5299b61974fb2e60d04ccda
Alexander Dadyka <sanekokokok@gmail.com>
2024-04-04 18:05:05 +0300

    Add: task #6_1

---
+++ t_6/main_1.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	atomic2 "sync/atomic"
+	"time"
+)
+
+/*
+Реализовать все возможные способы остановки выполнения горутины.
+
+Через глобальную переменную
+*/
+
+func main() {
+	var (
+		waitGroup  sync.WaitGroup
+		atomicBool atomic2.Bool
+	)
+
+	waitGroup.Add(1)
+
+	go func() {
+		for atomicBool.Load() == false {
+			fmt.Println("Goroutine run")
+			time.Sleep(time.Second)
+		}
+		fmt.Println("Goroutine stop")
+		waitGroup.Done()
+	}()
+
+	time.Sleep(3 * time.Second)
+
+	atomicBool.Store(true)
+
+	waitGroup.Wait()
+}
+
+/*
+Goroutine run
+Goroutine run
+Goroutine run
+Goroutine stop
+*/

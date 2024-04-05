24008c7ba2a8ef5f851951ae3abd48017be988a4
Alexander Dadyka <sanekokokok@gmail.com>
2024-04-05 20:50:42 +0300

    Add: task #13_1

---
+++ t_13/main_1.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+/*
+Поменять местами два числа без создания временной переменной.
+*/
+
+func main() {
+	variable1 := 3
+	variable2 := 7
+
+	fmt.Println("\nVariable #1\n", variable1, "\nVariable #2\n", variable2)
+
+	variable1, variable2 = variable2, variable1
+
+	fmt.Println("\nVariable #1\n", variable1, "\nVariable #2\n", variable2)
+}
+
+/*
+Variable #1
+ 3
+Variable #2
+ 7
+
+Variable #1
+ 7
+Variable #2
+ 3
+*/

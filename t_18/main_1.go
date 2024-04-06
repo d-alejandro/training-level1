c62bf60d3a896ee88c7869e5b43f518311fd8379
Alexander Dadyka <sanekokokok@gmail.com>
2024-04-07 02:52:18 +0300

    Add: task#18_1

---
+++ t_18/main_1.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+/*
+Реализовать структуру-счетчик, которая будет инкрементироваться в
+конкурентной среде. По завершению программа должна выводить итоговое
+значение счетчика.
+*/
+
+type AtomicCounter struct {
+	atomic atomic.Uint64
+}
+
+func NewAtomicCounter() *AtomicCounter {
+	return &AtomicCounter{}
+}
+
+func (atomicCounter *AtomicCounter) increment() {
+	atomicCounter.atomic.Add(1)
+}
+
+func (atomicCounter *AtomicCounter) getTotal() uint64 {
+	return atomicCounter.atomic.Load()
+}
+
+func main() {
+	var waitGroup sync.WaitGroup
+
+	atomicCounter := NewAtomicCounter()
+
+	for i := 0; i < 10; i++ {
+		waitGroup.Add(1)
+
+		go func(atomicCounter *AtomicCounter) {
+			for x := 0; x < 100; x++ {
+				atomicCounter.increment()
+			}
+			waitGroup.Done()
+		}(atomicCounter)
+	}
+
+	waitGroup.Wait()
+
+	fmt.Println("Total:", atomicCounter.getTotal())
+}
+
+/*
+Total: 1000
+*/

1777c35a84c5450b9b65bafe3b40694519db55de
Alexander Dadyka <sanekokokok@gmail.com>
2024-04-07 03:08:22 +0300

    Add: task#18_2

---
+++ t_18/main_2.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+/*
+Реализовать структуру-счетчик, которая будет инкрементироваться в
+конкурентной среде. По завершению программа должна выводить итоговое
+значение счетчика.
+*/
+
+type Counter struct {
+	sync.Mutex
+	total uint64
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func (counter *Counter) increment() {
+	counter.Lock()
+	defer counter.Unlock()
+	counter.total++
+}
+
+func (counter *Counter) getTotal() uint64 {
+	return counter.total
+}
+
+func main() {
+	var waitGroup sync.WaitGroup
+
+	counter := NewCounter()
+
+	for i := 0; i < 10; i++ {
+		waitGroup.Add(1)
+		go runCounter(counter, &waitGroup)
+	}
+
+	waitGroup.Wait()
+
+	fmt.Println("Total:", counter.getTotal())
+}
+
+func runCounter(counter *Counter, waitGroup *sync.WaitGroup) {
+	for x := 0; x < 100; x++ {
+		counter.increment()
+	}
+	waitGroup.Done()
+}
+
+/*
+Total: 1000
+*/

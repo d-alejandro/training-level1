4cfad646590dddd150292a4ca224de30e335dfc9
Alexander Dadyka <sanekokokok@gmail.com>
2024-04-07 15:10:57 +0300

    Add: task#20

---
+++ t_20/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+/*
+Разработать программу, которая переворачивает слова в строке.
+Пример: «snow dog sun — sun dog snow».
+*/
+
+func main() {
+	fmt.Println("Введите строку:")
+
+	reader := bufio.NewReader(os.Stdin)
+	inputString, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	inputString = strings.TrimSuffix(inputString, "\n")
+	stringArray := strings.Split(inputString, " ")
+
+	count := len(stringArray)
+	lastIndex := count - 1
+	middleIndex := lastIndex / 2
+
+	for key, value := range stringArray {
+		if key <= middleIndex {
+			stringArray[lastIndex-key], stringArray[key] = value, stringArray[lastIndex-key]
+		} else {
+			break
+		}
+	}
+
+	outputString := strings.Join(stringArray, " ")
+
+	fmt.Println(outputString)
+}
+
+/*
+Введите строку:
+▁▂▃▄▅▆▇█ snow dog sun
+sun dog snow ▁▂▃▄▅▆▇█
+*/

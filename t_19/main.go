081ba0052254ef721bc6b587fa65332014193a3f
Alexander Dadyka <sanekokokok@gmail.com>
2024-04-07 13:14:56 +0300

    Add: task#19

---
+++ t_19/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+/*
+Разработать программу, которая переворачивает подаваемую на ход строку
+(например: «главрыба — абырвалг»). Символы могут быть unicode.
+*/
+
+func main() {
+	var inputString string
+
+	fmt.Println("Введите строку:")
+
+	if _, err := fmt.Scan(&inputString); err != nil {
+		log.Fatal(err)
+	}
+
+	arrayRune := []rune(inputString)
+
+	runeCount := len(arrayRune)
+	lastIndex := runeCount - 1
+	middleIndex := lastIndex / 2
+
+	for key, value := range arrayRune {
+		if key <= middleIndex {
+			arrayRune[lastIndex-key], arrayRune[key] = value, arrayRune[lastIndex-key]
+		} else {
+			break
+		}
+	}
+
+	fmt.Println(string(arrayRune))
+}
+
+/*
+Введите строку:
+❶❷❸ЫмаldJQ→↓♛♞♫▁▂▃▄▅▆▇█
+█▇▆▅▄▃▂▁♫♞♛↓→QJdlамЫ❸❷❶
+*/
